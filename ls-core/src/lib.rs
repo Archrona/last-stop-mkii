f95f62539cffefba5b9ca8674a5b750d8f7723bd
mame <mamecccci@gmail.com>
2021-04-01 11:04:06 -0400

    Verify unit testing for rust

--- ls-core/src/lib.rs
+++ ls-core/src/lib.rs
@@ -2,7 +2,25 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+pub fn initialize() {
+    utils::set_panic_hook();
+}
+
 #[wasm_bindgen]
 pub fn dbl(x: f64) -> f64 {
     return x * 2.0;
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dbl() {
+        assert_eq!(dbl(10.0), 20.0);
+        assert_eq!(dbl(-6.0), -12.0);
+    }
 }
